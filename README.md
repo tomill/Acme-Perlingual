d3a4c6f5a4642a932c796cbd473c4570cb5fff2e
Naoki Tomita <n.tomita@gmail.com>
2014-08-31 17:15:06 +0900

    minil init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::P2P - It's new $module
+
+# SYNOPSIS
+
+    use Acme::P2P;
+
+# DESCRIPTION
+
+Acme::P2P is ...
+
+# LICENSE
+
+Copyright (C) Naoki Tomita.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Naoki Tomita <n.tomita@gmail.com>
---
+++ lib/Acme/P2P.pm
@@ -0,0 +1,39 @@
+package Acme::P2P;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::P2P - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::P2P;
+
+=head1 DESCRIPTION
+
+Acme::P2P is ...
+
+=head1 LICENSE
+
+Copyright (C) Naoki Tomita.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Naoki Tomita E<lt>n.tomita@gmail.comE<gt>
+
+=cut
+
